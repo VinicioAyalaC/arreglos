f830bf90945c32590ae46327811f211298d51184
VinicioAyalaC <vhac2311@gmail.com>
2026-04-30 21:59:35 -0500

    Fin de tarea

--- arreglos.js
+++ arreglos.js
@@ -1,55 +1,111 @@
-let edadesIzquierdo = []; //crear un arreglo
-let edadesDerecho =[];    //crear un arreglo
+let edadesIzquierdo = [];
+let edadesDerecho = [];
 
-function agrefarEdad(){
+// #2: Funcion agregarEdad
+function agregarEdad() {
     let edad = parseInt(document.getElementById("edad").value);
-    if(!isNaN(edad)){   //validamos q edad sea diferente de null
+    if (!isNaN(edad) && edad != 0 && edad > 0) {
         edadesIzquierdo.push(edad);
-    }else{
-        alert("Por favor ingrese un numero válido");
+        pintarArregloIzquierda();        
+        document.getElementById("edad").value = "";
+    } else {
+        alert("⚠️ Ingrese un número valido!");
     }
-
-    pintarArregloIzquierdo();
-
 }
 
 
-function eliminarIzquierdo(indice){
-    edadesIzquierdo.splice(indice,1);  // splice(i, 1) elimina, i=indice, 1=# a eliminar
-    pintarArregloIzquierdo();
-}
+//*********IZQUIERDA********************/
 
-
-
-function pintarArregloIzquierdo(){
+// #3: funcion pintarArregloIzquierdo 
+function pintarArregloIzquierda() {
     let tbody = document.getElementById("tablaIzquierda");
-    let contenidoTabla="";
+    let contenidoTabla = "";
     let edadRecuperada;
-
-    for(let i=0; i<edadesIzquierdo.length; i++ ){
-        edadRecuperada=edadesIzquierdo[i];
-        contenidoTabla+="<tr>";
-        contenidoTabla+="<td>"+edadRecuperada+"</td>";
-        contenidoTabla+="<td><button class='btn-eliminar' onclick='eliminarIzquierdo("+i+")'>Eliminar</button></td> <td><button class='btn-mover'>➜</button> </td>";
-        contenidoTabla+="</tr>";
+    //recorrer edades izquierda
+    for (let i = 0; i < edadesIzquierdo.length; i++) {
+        edadRecuperada = edadesIzquierdo[i]
+        contenidoTabla += "<tr>";
+        contenidoTabla += "<td>" + edadRecuperada + "</td>";
+        contenidoTabla += "<td> <button class='btn-eliminar' onclick='eliminarIzquierdo(" + i + ")'>Eliminar</button></td>";
+        contenidoTabla += "<td> <button class='btn-mover' onclick='moverHaciaDerecha(" + i + ")'> ➜ </button></td>"
+        contenidoTabla += "</tr>";
     }
+    tbody.innerHTML = contenidoTabla;
+}
 
-    tbody.innerHTML=contenidoTabla;
+// #4: funcion eliminar izquierdo
+function eliminarIzquierdo(indice) {
+    edadesIzquierdo.splice(indice, 1);  //.splice(indice,1)  borra(indice arreglo, # a borrar)
+    pintarArregloIzquierda();
+}
+
+// #8: funcion moverHaciaIzquierda
+function moverHaciaIzquierda(indice) {
+    let valorEdad = edadesDerecho[indice]; //obtener valor edad
+    edadesIzquierdo.push(valorEdad); //agregar valor edad
+    edadesDerecho.splice(indice, 1); // eliminar valor edad
+    pintarArregloIzquierda();
+    pintarArregloDerecha();
 }
 
 
-function pintarArregloDerecho(){
-    let tbody = document.getElementById("tablaDerecha");
-    let contenidoTabla="";
-    let edadRecuperada;
 
-    for(let i=0; i<edadesIzquierdo.length; i++ ){
-        edadRecuperada=edadesIzquierdo[i];
-        contenidoTabla+="<tr>";
-        contenidoTabla+="<td>"+edadRecuperada+"</td>";
-        contenidoTabla+="<td><button class='btn-eliminar' onclick='eliminarIzquierdo("+i+")'>Eliminar</button></td> <td><button class='btn-mover'>➜</button> </td>";
-        contenidoTabla+="</tr>";
+function moverTodoHaciaIzquierda() {
+    let valorEdad;
+    for (let i = 0; i < edadesDerecho.length; i++) {
+        valorEdad = edadesDerecho[i]; //obtener valor edad
+        edadesIzquierdo.push(valorEdad); //agregar valor edad
     }
+    edadesDerecho = []; //limpiar arreglo derecho
+    pintarArregloIzquierda();
+    pintarArregloDerecha();
+}
 
-    tbody.innerHTML=contenidoTabla;
+
+
+//*********DERECHA********************/
+
+// #5: funcion pintarArregloDerecha
+function pintarArregloDerecha() {
+    let tbody = document.getElementById("tablaDerecha");
+    let contenidoTabla = "";
+    let edadRecuperada;
+    //recorrer edades derecha
+    for (let i = 0; i < edadesDerecho.length; i++) {
+        edadRecuperada = edadesDerecho[i]
+        contenidoTabla += "<tr>";
+        contenidoTabla += "<td> <button class='btn-mover' onclick='moverHaciaIzquierda(" + i + ")'> ⬅ </button></td>"
+        contenidoTabla += "<td>" + edadRecuperada + "</td>";
+        contenidoTabla += "<td> <button class='btn-eliminar' onclick='eliminarDerecho(" + i + ")'>Eliminar</button></td>";
+        contenidoTabla += "</tr>";
+    }
+    tbody.innerHTML = contenidoTabla;
+}
+
+// #6: funcion eliminarDerecho
+function eliminarDerecho(indice) {
+    edadesDerecho.splice(indice, 1);
+    pintarArregloDerecha();
+}
+
+// #7: funcion moverHaciaDerecha
+function moverHaciaDerecha(indice) {
+    let valorEdad = edadesIzquierdo[indice]; //obtener valor edad
+    edadesDerecho.push(valorEdad); //agregar valor edad
+    edadesIzquierdo.splice(indice, 1); // eliminar valor edad
+    pintarArregloDerecha();
+    pintarArregloIzquierda();
+}
+
+
+ 
+function moverTodoHaciaDerecha() {
+    let valorEdad;
+    for (let i = 0; i < edadesIzquierdo.length; i++) {
+        valorEdad = edadesIzquierdo[i]; //obtener valor edad
+        edadesDerecho.push(valorEdad); //agregar valor edad
+    }
+    edadesIzquierdo = []; //limpiar arreglo izquierdo
+    pintarArregloIzquierda();
+    pintarArregloDerecha();
 }
